5835ffb8118d351a42017ef02e8923b563874050
Alex <alex@zeitgeist.se>
2024-11-10 11:33:40 +0100

    feat: Enhance server data fetching with additional server details and improved output

--- main.go
+++ main.go
@@ -17,19 +17,25 @@ const interfaceName = "norrvpn01"
 func main() {
 	flag.Parse()
 	function := flag.Arg(0)
-	var host, key string
+
 	switch function {
 	case "up":
+		var host, key string
+		var server Server
 		if flag.NArg() == 2 {
-			host, key = FetchServerData(getCountryCode(flag.Arg(1)))
+			host, key, server = FetchServerData(getCountryCode(flag.Arg(1)))
 		} else {
-			host, key = FetchServerData(-1)
+			host, key, server = FetchServerData(-1)
 		}
 		privateKey := fetchOwnPrivateKey(getToken())
-		fmt.Printf("Country code: %s\n", flag.Arg(1))
+		fmt.Printf("Server name: %s\n", server.Name)
+		fmt.Printf("Country: %s (%s)\n", server.Locations[0].Country.Name, server.Locations[0].Country.Code)
+		fmt.Printf("City: %s\n", server.Locations[0].Country.City.Name)
+		fmt.Printf("Load: %d%%\n", server.Load)
+		fmt.Printf("Status: %s\n", server.Status)
+		fmt.Printf("Hostname: %s\n", server.Hostname)
 		fmt.Printf("WG public key: %s\n", key)
 		fmt.Printf("WG private key: %s\n", privateKey)
-		fmt.Printf("WG host: %s\n", host)
 		execWGup(interfaceName, privateKey, key, host, defaultNordvpnAddress)
 	case "down":
 		execWGdown(interfaceName, defaultNordvpnAddress)
